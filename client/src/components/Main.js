7657e3253625013ad8b22cad84ca9af5ab504ae6
Seth King <sethkingwriter@gmail.com>
2023-12-17 18:53:57 -0500

    front end work

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -3,53 +3,25 @@ import React from 'react'
 function Main() {
   return (
     <div>
-            <main class="p-4 md:ml-64 h-auto pt-20">
-                    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
-                        <div
-                            class="border-2 border-dashed border-gray-300 rounded-lg dark:border-gray-600 h-32 md:h-64"
-                        ></div>
-                        <div
-                            class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64"
-                        ></div>
-                        <div
-                            class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64"
-                        ></div>
-                        <div
-                            class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64"
-                        ></div>
+            <main class="p-4 md:ml-64 h-screen	 pt-20">
+                    <div>
+                        <h2 style={{
+                            textAlign: 'initial',
+                            fontFamily: 'ui-sans-serif, system-ui, -apple-system',
+                            fontSize: '25px',
+                            marginTop: 0
+                        }}> Text Compare</h2>
                     </div>
-                    <div
+                    {/* <div
                         class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4"
-                    ></div>
+                    >
+                    </div> */}
                     <div class="grid grid-cols-2 gap-4 mb-4">
                         <div
-                            class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
+                            class="border-2 border-solid rounded-lg border-gray-300 dark:border-gray-600 h-96"
                         ></div>
                         <div
-                            class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
-                        ></div>
-                        <div
-                            class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
-                        ></div>
-                        <div
-                            class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
-                        ></div>
-                    </div>
-                    <div
-                        class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4"
-                    ></div>
-                    <div class="grid grid-cols-2 gap-4">
-                        <div
-                            class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
-                        ></div>
-                        <div
-                            class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
-                        ></div>
-                        <div
-                            class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
-                        ></div>
-                        <div
-                            class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
+                            class="border-2 border-solid rounded-lg border-gray-300 dark:border-gray-600 h-96"
                         ></div>
                     </div>
                 </main>
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
-function Sidebar() {
+function Left() {
   return (
     <div>
+                        <aside
+                    class="fixed top-0 left-0 z-40 w-64 h-screen pt-14 transition-transform -translate-x-full bg-white border-r border-gray-200 md:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+                    aria-label="Sidenav"
+                    id="drawer-navigation"
+                >
                     <div class="overflow-y-auto py-5 px-3 h-full bg-white dark:bg-gray-800">
                         <form action="#" method="GET" class="md:hidden mb-2">
                             <label for="sidebar-search" class="sr-only">Search</label>
@@ -549,8 +554,9 @@ function Sidebar() {
                             </ul>
                         </div>
                     </div>
+                </aside>
     </div>
   )
 }
 
-export default Sidebar
+export default Left
